384a6b80d0125b00af81de821733849b25c740f3
Egor <78965361+Kloun456@users.noreply.github.com>
2021-05-12 13:41:08 +0300

    Update Hero.h

--- Hero.h
+++ Hero.h
@@ -22,6 +22,7 @@ public:
     virtual bool Reinforced_blow(Monster* monster) = 0;
     virtual bool Super_blow(Monster* monster) = 0;
     virtual void Show_atacks() = 0;
+    virtual void Taking_xp(Monster* monster) = 0;
     void Block()
     {
         this->armor += 2*lvl;
@@ -31,22 +32,23 @@ public:
         this->armor -= 2*lvl;
     }
     void Use_subject();
-    void Show_subjects();
-    void Add_subject();
+    bool Show_inventory();
+    void Add_subject(Subject *subject);
     void Start_position(Location* loc);
     void Change_position(Location* loc);
     void Taking_damage(Monster* monster);
-    void Taking_xp(Monster* monster);
     void Show_stat();
     bool Fight(Monster* monster);
     int Get_lvl() { return lvl; }
     int Get_hp() { return hp; }
 protected:
     Weapon *weapon;
-    list<Subject> subjects;
+    vector<Subject *> subjects; // inventory 5 items
     friend class Location;
     friend class Castle;
     friend class Forest;
-    int xp, lvl, hp, mp, atack, armor, pos_i, pos_j, temp_pos_i, temp_pos_j;
+    friend class MP_potion;
+    friend class HP_potion;
+    int xp, lvl, hp, mp, atack, armor, pos_i, pos_j, temp_pos_i, temp_pos_j, temp_hp, temp_mp;
 };
 
