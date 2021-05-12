9a9582c754061b2a0ea246443919111ccd9b442f
Egor <78965361+Kloun456@users.noreply.github.com>
2021-05-12 13:45:43 +0300

    Update Location.h

--- Location.h
+++ Location.h
@@ -1,8 +1,8 @@
-#pragma once
 #include <iostream>
 #include <string>
 #include "Monster.h"
 
+class Subject;
 class Hero;
 
 using namespace std;
@@ -14,16 +14,17 @@ public:
     Location(Hero *hero);
     ~Location();
     virtual void Trap_damage(Hero *hero) = 0;
-    void Show_location();
+    void Show_location(); 
     void Show_admin_location();
     int Check_position(Hero *hero);
-    void Get_player_position(int& position_i, int& position_j);
+    void Get_player_position(int& position_i, int& position_j); 
+    Subject *Get_treasure();
     int Get_Size() { return this->n; }
     int Get_lvl() { return lvl; }
-    int Get_quantity_monsters() { return m; }
     void Decrease_monsters() { this->m--; }
 protected:
     string** field,objects[4] = { "monster","empty","trap","treasure" };
+    string Treasures[2] = {"MP potion","HP potion"};
     friend class Hero;
     int m = 0, trap = 0, tres = 0, n, lvl; // m - quantity monsters, tres - treasure
 };
