73aea7c2c355181efe1a424d15c7828e2de5126d
Egor <78965361+Kloun456@users.noreply.github.com>
2021-05-12 13:54:38 +0300

    Update Warrior.cpp

--- Warrior.cpp
+++ Warrior.cpp
@@ -5,13 +5,13 @@ void Warrior::Show_atacks()
     switch (lvl)
     {
     case 1:
-        cout << "1 - Blow\n2 - Reinforced blow(lock)\n3 - Super blow(lock)\n4 - Block\n";
+        cout << "1 - Blow\n2 - Reinforced blow(lock)\n3 - Super blow(lock)\n4 - Block\n5 - Use inventory\n";
         break;
     case 2:
-        cout << "1 - Blow\n2 - Reinforced blow (5 mp)\n3 - Super blow(lock)\n4 - Block\n";
+        cout << "1 - Blow\n2 - Reinforced blow (5 mp)\n3 - Super blow(lock)\n4 - Block\n5 - Use inventory\n";
         break;
     case 3:
-        cout << "1 - Blow\n2 - Reinforced blow (5 mp)\n3 - Super blow (20 mp)\n4 - Block\n";
+        cout << "1 - Blow\n2 - Reinforced blow (5 mp)\n3 - Super blow (20 mp)\n4 - Block\n5 - Use inventory\n";
         break;
     }
 }
@@ -56,5 +56,21 @@ bool Warrior::Super_blow(Monster* monster)
             return 0;
         }
         return 1;
+    }
+}
+
+void Warrior::Taking_xp(Monster* monster)
+{
+    xp += monster->lvl * 50;
+    if (xp >= lvl * 100)
+    {
+        xp = xp - lvl * 100;
+        lvl++;
+        hp = temp_hp + 50;
+        mp = temp_mp + 20;
+        temp_hp = hp;
+        temp_mp = mp;
+        atack += 2;
+        armor += 3;
     }
 }
